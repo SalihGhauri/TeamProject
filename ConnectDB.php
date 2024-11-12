840c9b52cae689e14e4e2ebdcb3ec98b9aa41949
N-Marer <230208856@aston.ac.uk>
2024-11-12 11:50:22 +0000

    file to connect to database

---
+++ ConnectDB.php
@@ -0,0 +1,15 @@
+<?php
+$dbname = "cs2team10_db";
+$dbhost = "localhost";
+$username = "cs2team10";
+$password = "3WqYX34Scob8B1n";
+
+try {
+  $conn = new PDO("mysql:host=$dbhost;dbname=dbname", $username, $password);
+  //sets the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
