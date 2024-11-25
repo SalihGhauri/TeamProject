6e905ed75079ad45265d279aa59110f962f1c088
Armada678 <159663722+Armada678@users.noreply.github.com>
2024-11-25 20:44:14 +0000

    added most of the backend for login

--- ConnectDB.php
+++ ConnectDB.php
@@ -5,7 +5,7 @@ $username = "cs2team10";
 $password = "3WqYX34Scob8B1n";
 
 try {
-  $conn = new PDO("mysql:host=$dbhost;dbname=dbname", $username, $password);
+  $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
   //sets the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
