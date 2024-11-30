12635c00b568c0bb417fc50ef9a71e26f0438528
Kirithy-n <Kirunaganathan14@gmail.com>
2024-11-30 03:10:19 +0000

    updated sports html code

--- JAVASCRIPT/filter.js
+++ JAVASCRIPT/filter.js
@@ -4,18 +4,18 @@ function redirectToCategory(category) {
             window.location.href = 'basketball.html';
             break;
         case 'football':
-            window.location.href = 'football.html'; 
+            window.location.href = 'football.html';
             break;
         case 'rugby':
-            window.location.href = 'rugby.html'; 
+            window.location.href = 'rugby.html';
             break;
         case 'volleyball':
-            window.location.href = 'volleyball.html'; 
+            window.location.href = 'volleyball.html';
             break;
         case 'golf':
-            window.location.href = 'golf.html'; 
+            window.location.href = 'golf.html';
             break;
         default:
-            alert('Invalid category selected'); 
+            alert('Invalid category selected');
     }
 }
